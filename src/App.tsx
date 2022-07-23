a233fd6323eb8b836fce5561194360ad60fe4f12
Augusto Marsola <augusto.marsola@live.com>
2022-07-23 16:23:48 -0300

    Added Theme provider to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+import { Button } from "./components/Button";
+
 export function App() {
-  return <h1>Ignite Timer - Aula</h1>;
+  return (
+    <>
+      <h1>Ignite Timer - Aula</h1>
+      <Button>Teste</Button>
+      <Button variant="danger">Teste</Button>
+      <Button variant="success">Teste</Button>
+      <Button variant="secondary">Teste</Button>
+      <Button variant="primary">Teste</Button>
+    </>
+  );
 }
