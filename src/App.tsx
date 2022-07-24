a4fa6f08b8ad5ec82f31d8373d1aea20dcf8fac9
Augusto Marsola <augusto.marsola@live.com>
2022-07-23 21:00:58 -0300

    Added home and history pages with react router dom component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
+import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Ignite Timer - Aula</h1>
-      <Button>Teste</Button>
-      <Button variant="danger">Teste</Button>
-      <Button variant="success">Teste</Button>
-      <Button variant="secondary">Teste</Button>
-      <Button variant="primary">Teste</Button>
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
